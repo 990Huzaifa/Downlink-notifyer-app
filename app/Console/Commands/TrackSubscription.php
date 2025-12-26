af35494012605ec79da39e7088b4cff7cae105f1
990Huzaifa <106827131+990Huzaifa@users.noreply.github.com>
2025-12-26 11:47:23 +0500

    Fix duration value type in SiteLink update to string

--- app/Console/Commands/TrackSubscription.php
+++ app/Console/Commands/TrackSubscription.php
@@ -47,7 +47,7 @@ class TrackSubscription extends Command
                 SiteLink::where('user_id', $subscription->user_id)
                     ->update([
                         'is_disabled' => true,
-                        'duration' => 86400,
+                        'duration' => "86400",
                         'notify_email' => 0,
                         'notify_sms' => 0,
                     ]);
