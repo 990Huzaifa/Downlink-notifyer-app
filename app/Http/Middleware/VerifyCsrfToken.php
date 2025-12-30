8abc240d297361b3672eee6ad068a5781b96b07b
990Huzaifa <106827131+990Huzaifa@users.noreply.github.com>
2025-12-30 15:49:44 +0500

    test case again

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'broadcasting/auth',
+        '/broadcasting/auth',
+        'api/*',  // Agar yeh already hai to theek hai
     ];
 }
