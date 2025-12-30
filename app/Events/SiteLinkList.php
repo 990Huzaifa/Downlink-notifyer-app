2b4edc51eca6278b69d18ffc8de64d09e2e61642
990Huzaifa <106827131+990Huzaifa@users.noreply.github.com>
2025-12-30 12:22:49 +0500

    testting event

---
+++ app/Events/SiteLinkList.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\SiteLink;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SiteLinkList implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $userId;
+    public $siteLinks;
+
+    public function __construct($userId)
+    {
+        $this->userId = $userId;
+        $this->siteLinks = SiteLink::where('user_id', $userId)->get();
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn()
+    {
+        return [new PresenceChannel('private-user.' . $this->userId)]; // Broadcasting to the user-specific channel
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'data' => $this->siteLinks,
+        ];
+    }
+}
